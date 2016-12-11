33c44e4d6add19af887b26d138d9055f1b816190
hoanhuocbinh <hoang.luong@sjsu.edu>
2016-12-10 18:25:30 -0800

    4 commit.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var app = express();
 var port = process.env.PORT || 1337;
+
+const START = "start";
+const ROCK = "r";
+const PAPER = "p";
+const SCISSORS = "s";
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,17 +16,49 @@ app.use(bodyParser.json())
 
 app.post('/startGame', function(req, res) {
   var userName = req.body.user_name;
-  var userMove = req.body.text;
+  var userID = req.body.user_id;
+  var input = req.body.text;
+  var responseText;
+  if (input != ROCK && input != PAPER && input != SCISSORS)
+  {
+    responseText = "Invalid input! Please enter [r]ock, [p]aper or [s]cissors.";
+  }
+  else {
+    // Call GetBestMove API with userName
+    // Body: { user_id: "12345" }
+    var botMove = 's';
+    responseText = compare(input, botMove);
+    // Call Cloudant API with userName, Usermove, Botmove
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("GET", "https://rps-cloudant-db.mybluemix.net/api/match", false)
+    // xhr.send();
+
+    // Body: { user_id: "12345", user_move: "R", botMove: "P" }
+  }
   var botPayload = {
-    text : userMove
+    text : responseText
   };
-  // Loop otherwise..
+
   if (userName !== 'slackbot') {
     return res.status(200).json(botPayload);
   } else {
     return res.status(200).end();
   }
 });
+
+function compare (userMove, botMove)
+{
+  if ((userMove === ROCK && botMove === SCISSORS) ||
+      (userMove === PAPER && botMove === ROCK) ||
+      (userMove === SCISSORS && botMove === PAPER))
+  {
+    return "You win!";
+  } else if (userMove === botMove) {
+    return "Tie!";
+  } else {
+    return "You lose!";
+  }
+}
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
 
